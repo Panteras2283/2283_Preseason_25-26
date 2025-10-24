1a8763870b551c6f6751be5d5a0226d53634a5f5
Santiago Sepúlveda <60012979+Sepu2002@users.noreply.github.com>
2025-10-24 07:00:55 -0600

    Update RobotContainer.java

--- SwerveWithPathPlanner/src/main/java/frc/robot/RobotContainer.java
+++ SwerveWithPathPlanner/src/main/java/frc/robot/RobotContainer.java
@@ -123,12 +123,6 @@ public class RobotContainer {
             forwardStraight.withVelocityX(-0.5).withVelocityY(0))
         );
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-       /*  driver.back().and(driver.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        driver.back().and(driver.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        driver.start().and(driver.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        driver.start().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));*/
 
         // reset the field-centric heading on left bumper press
         driver.back().whileTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
